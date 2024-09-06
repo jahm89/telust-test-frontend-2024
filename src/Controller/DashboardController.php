9c6a1e7331a005437d62317de17da2eaabbd0c1b
Jeovanni Hernandez <jeovanni.jahm@gmail.com>
2024-09-06 12:39:35 -0600

    adding raw data and pie chart

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\HeaderProcess;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Entity\HeaderProcess;
+use App\Entity\DetailProcess;
 
 
 class DashboardController extends AbstractController
@@ -25,5 +29,27 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'groups' => $this->entityManager->getRepository(HeaderProcess::class)->findAll()
         ]);
+    }
+
+    #[Route('/get-group-data', name: 'app_get_group_data')]
+    public function getGroupData(Request $request, SerializerInterface $serializer): JsonResponse
+    {
+        $groupId = $request->get('groupId');
+        $headerProcess = $this->entityManager->getRepository(HeaderProcess::class)->find($groupId);
+        $data = $this->entityManager->getRepository(DetailProcess::class)->findOneBy(['header_process' => $headerProcess]);
+        $data = $data->getData();
+
+        foreach($data['osTotal'] as $key => $os) {
+            $osFormattedData[] = [
+                'name' => $key,
+                'y' => $os
+            ];
+        }
+
+        return new JsonResponse([
+            'data' => $data,
+            'osFormattedData' => $osFormattedData
+            ]
+        );
     }
 }
